9a23a11edeebbe7726aede3a0c4b956f91deab23
Muhammadqodir <mirzaolimovinfinity@gmail.com>
2026-06-03 12:16:28 +0500

    homework_9 davomi

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import index, course_students, student_detail
+from .views import home, course_students, student_detail,create_course,update_course,create_student,update_student,delete_course,delete_student
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
     path('course/<int:course_id>/students/', course_students, name='course_students'),
     path('student/<int:student_id>/', student_detail, name='student_detail'),
+
+    path('add/course/', create_course, name='create_student'),
+    path('add/student/',create_student,name='create_student'),
+    path('course/update/<int:course_id>/',update_course,name='update_course'),
+    path('student/update/<int:student_id>/',update_student,name='update_student'),
+    path('course/delete/<int:course_id>/',delete_course,name='delete_course'),
+    path('student/delete/<int:student_id>/',delete_student,name='delete_student')
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Course, Student
+from .forms import CourseForm,StudentForm
 
-
-def index(request):
+def home(request):
     courses = Course.objects.all()
     students = Student.objects.all()
     return render(request, 'core/index.html', {'courses': courses, 'students': students})
@@ -18,3 +18,107 @@ def student_detail(request, student_id):
     courses = Course.objects.all()
     student = get_object_or_404(Student, id=student_id)
     return render(request, 'core/student_detail.html', {'courses': courses, 'student': student})
+
+# --------------------------- start Course ------------------------------------------
+def create_course(request):
+    if request.method == 'POST':
+        form = CourseForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CourseForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request,'core/add_course.html',context)
+
+# --------------------------- end Course --------------------------------------------
+
+
+# --------------------------- update Course ---------------------------------------------
+def update_course(request,course_id):
+    course = Course.objects.get(pk=course_id)
+    if request.method == 'POST':
+        form = CourseForm(data=request.POST,instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        form = CourseForm(instance=course)
+
+    context = {
+        'form': form
+    }
+
+    return render(request,'core/update_course.html',context)
+# --------------------------- end update Course ------------------------------------------
+
+
+# --------------------------- create  Student --------------------------------------------
+def create_student(request):
+    if request.method == 'POST':
+        form = StudentForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = StudentForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request,'core/add_student.html',context)
+# --------------------------- end  Student -----------------------------------------------
+
+# --------------------------- update Student ---------------------------------------------
+def update_student(request,student_id):
+    student = Student.objects.get(pk=student_id)
+    if request.method == 'POST':
+        form = StudentForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = StudentForm(instance=student)
+
+    context = {
+        'form': form
+    }
+
+    return render(request,'core/update_student.html',context)
+# --------------------------- end update Student -----------------------------------------
+
+# --------------------------- delete Course   --------------------------------------------
+def delete_course(request,course_id):
+    course = Course.objects.get(pk=course_id)
+
+    if request.method == 'POST':
+        course.delete()
+        return redirect('home')
+
+    context = {
+        'course': course
+    }
+
+    return render(request,'core/delete_course.html',context)
+# --------------------------- end delete Course ------------------------------------------
+
+# --------------------------- delete Student  --------------------------------------------
+def delete_student(request,student_id):
+    student = Student.objects.get(pk=student_id)
+
+    if request.method == 'POST':
+        student.delete()
+        return redirect('home')
+
+    context = {
+        'student': student
+    }
+
+    return render(request,'core/delete_student.html',context)
+# --------------------------- end delete Student  ----------------------------------------
