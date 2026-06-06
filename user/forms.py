cdb68ab43353117228d86fbe63799ff1b18cfc20
Muhammadqodir <mirzaolimovinfinity@gmail.com>
2026-06-06 11:33:22 +0500

    homework_9 oxiri

---
+++ user/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
---
+++ user/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, AuthenticationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_view')
+    else:
+        form = RegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'user/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
